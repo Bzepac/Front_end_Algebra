7319868d0513283444db269d1e94c51b1bd27164
Bzepac <146201431+Bzepac@users.noreply.github.com>
2024-01-08 19:00:33 +0100

    n

--- JS/Fibonaci_funkcija/fibonacifunc.js
+++ JS/Fibonaci_funkcija/fibonacifunc.js
@@ -3,15 +3,16 @@ let zbroj = 1;
 let zbroj2 = 1;
 const niz = [];
 
-function counter(n) {
+function fibo(n) {
   niz[i + 1] = zbroj2;
   niz[i] = zbroj;
   zbroj = zbroj + zbroj2;
   zbroj2 = zbroj + zbroj2;
 
   i = i + 2;
-  if (i < n) return counter(n);
+  if (n % 2 == 1) niz.splice(n);
+  if (i < n) return fibo(n);
   else console.log(niz);
 }
 
-counter(11);
+fibo(14);
