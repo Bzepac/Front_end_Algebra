2a016fae265bd6cf794b39ab69d8043df99f67f4
Bzepac <146201431+Bzepac@users.noreply.github.com>
2024-01-11 23:17:05 +0100

    csstodo

--- JS/Todolist/todojs.js
+++ JS/Todolist/todojs.js
@@ -1,15 +1,20 @@
-let i = 1;
 document.querySelector(".addButton").addEventListener("click", function () {
   var task = document.querySelector(".int").value;
   var tekst = "    " + task + "    ";
   const li = document.createElement("li");
+
   const check = document.createElement("input");
   const remove = document.createElement("button");
   remove.className = "removeButton";
-  li.className = "lii";
 
   check.type = "checkbox";
-
+  check.addEventListener("click", function () {
+    if (check.checked) {
+      li.className = "done";
+    } else {
+      li.className = "";
+    }
+  });
   li.append(check);
 
   li.appendChild(document.createTextNode(tekst));
