661206a5bbdaf04ea4e6b894d4182434c15cbfae
Bzepac <146201431+Bzepac@users.noreply.github.com>
2024-01-21 18:36:30 +0100

    fetch

--- JS/fetch/search.js
+++ JS/fetch/search.js
@@ -1,10 +1,18 @@
 let i = 0;
 let trazilica = document.querySelector("#search");
+let loading = document.createElement("p");
+
+document.querySelector("body").appendChild(loading);
+const allp = document.querySelectorAll("p");
+allp.forEach((p) => {
+  p.className = "hide";
+});
 
 trazilica.addEventListener("input", function () {
   const textinput = trazilica.value.toLowerCase();
   console.log(textinput);
   const p = document.querySelectorAll("p");
+
   p.forEach((p) => {
     const username = p.textContent.toLowerCase();
     if (username.includes(textinput)) {
@@ -14,7 +22,8 @@ trazilica.addEventListener("input", function () {
     }
   });
 });
-fetch("https://jsonplaceholder.typicode.com/users")
+
+fetch("https://randomuser.me/api/?results=2000")
   .then((response) => {
     if (!response.ok) {
       throw new Error("Nev valja");
@@ -23,13 +32,18 @@ fetch("https://jsonplaceholder.typicode.com/users")
     }
   })
   .then((data) => {
-    data.forEach((data) => {
+    document.querySelector(".labelo").textContent = "Type to search";
+
+    data.results.forEach((artist) => {
+      console.log(data.results);
       let text = document.createElement("p");
+      text.className = "hide";
 
-      const firstUsername = data.name;
-
-      text.textContent = firstUsername;
-      console.log(data);
+      const firstUsername = artist.name.first;
+      const lastUsername = artist.name.last;
+      const gender = artist.gender;
+      text.textContent = firstUsername + " " + lastUsername + " " + gender;
+      console.log(artist.name.first);
       document.querySelector("body").appendChild(text);
     });
   })
