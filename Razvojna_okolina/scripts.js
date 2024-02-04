561c7613acd7f5e04d440e18f446420961f32b5c
Bzepac <146201431+Bzepac@users.noreply.github.com>
2024-02-04 11:52:55 +0100

    Folder_names

---
+++ Razvojna_okolina/scripts.js
@@ -0,0 +1,29 @@
+var cast = {
+  characters: [
+    {
+      name: "Jon Snow",
+      actor: "Kit Harington",
+      house: "Stark",
+    },
+    {
+      name: "Son Snow",
+      actor: "Kit Harington",
+      house: "Stark",
+    },
+    {
+      name: "Arya Stark",
+      actor: "Maisie Williams",
+      house: "Stark",
+    },
+    {
+      name: "Tyrion Lannister",
+      actor: "Peter Dinklage",
+      house: "Lannister",
+    },
+    {
+      name: "Daenerys Targaryen",
+      actor: "Emilia Clarke",
+      house: "Targaryen",
+    },
+  ],
+};
