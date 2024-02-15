377e1190b571e37fa1044a0a00978583082d1b72
Bzepac <146201431+Bzepac@users.noreply.github.com>
2024-02-15 12:07:31 +0100

    update

--- JS/fetch/search.js
+++ JS/fetch/search.js
@@ -1,16 +1,12 @@
 let trazilica = document.querySelector("#search");
 let loading = document.querySelector(".labelo");
-
 let resultsContainer = document.createElement("div");
 document.querySelector("body").appendChild(resultsContainer);
-
 trazilica.addEventListener("input", function () {
   const textinput = trazilica.value.toLowerCase();
   console.log(textinput);
   loading.textContent = "Loading...";
-
   resultsContainer.innerHTML = "";
-
   fetch(`https://itunes.apple.com/search?term=${textinput}&entity=song`)
     .then((response) => {
       if (!response.ok) {
@@ -24,11 +20,15 @@ trazilica.addEventListener("input", function () {
       reverseResults.forEach((result) => {
         console.log(result);
         let text = document.createElement("p");
+        const artistName = result.artistName;
+        const song = result.trackName;
+        text.textContent = artistName + " - " + song;
+        resultsContainer.appendChild(text);
+        loading.textContent = "";
       });
     })
     .catch((error) => {
       loading.textContent = "No results";
-
       console.error(error);
     });
 });
