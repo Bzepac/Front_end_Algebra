417df2bf459f71bff0c20504e12c34bb1f1cd20f
Bzepac <146201431+Bzepac@users.noreply.github.com>
2024-02-17 22:17:37 +0100

    Zadaca_conditionalrendering()

--- React/Practice/react_course/src/App.jsx
+++ React/Practice/react_course/src/App.jsx
@@ -1,11 +1,25 @@
+import reactImg from "../src/assets/react-core-concepts.png";
+import otherImg from "../src/assets/jsx-ui.png";
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+function randomImg() {
+  if (Math.random(1) > 0.5) {
+    return otherImg;
+  } else {
+    return reactImg;
+  }
+}
 function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={randomImg()} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
   );
@@ -14,7 +28,7 @@ function Header() {
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
--- React/Practice/zadaca react/src/App.jsx
+++ React/Practice/zadaca react/src/App.jsx
@@ -1,11 +1,25 @@
-import { ime, prezime, godine } from "./modules.jsx";
+import reactImg from "./assets/react-core-concepts.png";
+
+function conditionalRendering(props) {
+  if (props === 0) {
+    return "Prvi tekst";
+  } else if (props === 1) {
+    return "Drugi tekst";
+  } else {
+    return "";
+  }
+}
 
 function Header() {
+  const broj = Math.floor(Math.random() * 2);
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>{ime()}</h1>
-      <p>{prezime}</p>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>{conditionalRendering(broj)}</h1>
+      <p>
+        These are the React concepts you will need for almost any app you are
+        going to build!
+      </p>
     </header>
   );
 }
@@ -13,14 +27,8 @@ function Header() {
 function App() {
   return (
     <div>
-      <Header></Header>
-      <main>
-        <h2>
-          <br />
-
-          {godine}
-        </h2>
-      </main>
+      <Header />
+      <main></main>
     </div>
   );
 }
