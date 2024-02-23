21bd9046224fb44e3538e45a57c17d08f1b45f34
hyperv <hyperv@ucione.local>
2024-02-23 20:17:37 +0100

    react reausable components with props

--- React/Practice/react_course/src/App.jsx
+++ React/Practice/react_course/src/App.jsx
@@ -1,25 +1,23 @@
 import reactImg from "../src/assets/react-core-concepts.png";
-import otherImg from "../src/assets/jsx-ui.png";
+import { CORE_CONCEPTS } from "./data";
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-function randomImg() {
-  if (Math.random(1) > 0.5) {
-    return otherImg;
-  } else {
-    return reactImg;
-  }
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
 }
 function Header() {
   return (
     <header>
-      <img src={randomImg()} alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescriptions[genRandomInt(2)]} React concepts you will need for
-        almost any app you are going to build!
+        React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
@@ -29,8 +27,24 @@ function App() {
   return (
     <div>
       <Header />
-      <main>
-        <h2>Time to get started!</h2>
+      <main id="core-concepts">
+        <ul>
+          <CoreConcept
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            img={CORE_CONCEPTS[0].image}
+          ></CoreConcept>
+          <CoreConcept
+            title={CORE_CONCEPTS[1].title}
+            description={CORE_CONCEPTS[1].description}
+            img={CORE_CONCEPTS[1].image}
+          ></CoreConcept>
+          <CoreConcept
+            title={CORE_CONCEPTS[2].title}
+            description={CORE_CONCEPTS[2].description}
+            img={CORE_CONCEPTS[2].image}
+          ></CoreConcept>
+        </ul>
       </main>
     </div>
   );
