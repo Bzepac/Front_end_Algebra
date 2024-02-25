515fa649eb5b3277fe9e75a5a9ab66ebbd1e53a9
Bzepac <146201431+Bzepac@users.noreply.github.com>
2024-02-25 19:37:30 +0100

    changes

--- React/Practice/react_course/src/App.jsx
+++ React/Practice/react_course/src/App.jsx
@@ -1,28 +1,6 @@
-import reactImg from "../src/assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data";
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.img} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
-function Header() {
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
+import Header from "./components/header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 function App() {
   return (
     <div>
@@ -43,6 +21,11 @@ function App() {
             title={CORE_CONCEPTS[2].title}
             description={CORE_CONCEPTS[2].description}
             img={CORE_CONCEPTS[2].image}
+          ></CoreConcept>
+          <CoreConcept
+            title={CORE_CONCEPTS[3].title}
+            description={CORE_CONCEPTS[3].description}
+            img={CORE_CONCEPTS[3].image}
           ></CoreConcept>
         </ul>
       </main>
