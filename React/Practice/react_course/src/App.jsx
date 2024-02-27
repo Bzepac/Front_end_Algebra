0d1a21e7834ab8a2f74f450e78a0f59b89032c8c
Bzepac <146201431+Bzepac@users.noreply.github.com>
2024-02-27 19:38:18 +0100

    chganges

--- React/Practice/react_course/src/App.jsx
+++ React/Practice/react_course/src/App.jsx
@@ -1,7 +1,12 @@
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+import React, { useState } from "react";
 function App() {
+  function onSelect({ children }) {
+    <h3>{children}</h3>;
+  }
   return (
     <div>
       <Header />
@@ -29,6 +34,49 @@ function App() {
           ></CoreConcept>
         </ul>
       </main>
+
+      <section id="examples">
+        <menu>
+          <li>
+            <TabButton onSelect={onSelect}>Components</TabButton>
+          </li>
+          <li>
+            <TabButton onSelect={onSelect}>Jsx</TabButton>
+          </li>
+          <li>
+            <TabButton onSelect={onSelect}>Props</TabButton>
+          </li>
+          <li>
+            <TabButton onSelect={onSelect}>State</TabButton>
+          </li>
+        </menu>
+        <section id="tab-content">
+          <div>
+            <h3>
+              The core UI building block - compose the user interface by
+              combining multiple components.
+            </h3>
+          </div>
+          <div>
+            <h3>
+              Return (potentially dynamic) HTML(ish) code to define the actual
+              markup that will be rendered.
+            </h3>
+          </div>
+          <div>
+            <h3>
+              Make components configurable (and therefore reusable) by passing
+              input data to them.
+            </h3>
+          </div>
+          <div>
+            <h3>
+              React-managed data which, when changed, causes the component to
+              re-render & the UI to update.
+            </h3>
+          </div>
+        </section>
+      </section>
     </div>
   );
 }
